850455c34994e0f1a694f9a54898762c5878009d
Cody Marshall <codymstuntsx@gmail.com>
2023-05-16 07:19:55 -0700

    Added board file structure

--- pages/Board/Board.tsx
+++ pages/Board/Board.tsx
@@ -1,5 +1,34 @@
+import styles from "./Board.module.scss";
+import List from "./components/List";
+
+const testArray = [
+  {
+    id: 1,
+    name: "MVP Features",
+    description: "My Features",
+    labels: ["In Progress", "ASAP"],
+    createdBy: "Cody Marshall",
+    items: [
+      {
+        id: 1,
+        name: "Floating Navbar",
+        description: "Make box shadow super cool",
+      },
+      {
+        id: 2,
+        name: "Hover Buttons",
+        description: "Make them super clickable",
+      },
+    ],
+  },
+];
+
 export default function Board() {
   return (
-    <div>Board</div>
-  )
+    <div className={styles.board}>
+      {testArray.map((item) => (
+        <List key={item.name} item={item} />
+      ))}
+    </div>
+  );
 }
